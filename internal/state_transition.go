d85baf6e0d7aa223630dbdefdc83c03a20a1d72a
David Xiang <admin@mdavid.cn>
2024-07-09 00:33:31 +0800

    upt

--- internal/state_transition.go
+++ internal/state_transition.go
@@ -7,6 +7,7 @@ import (
 var (
 	New2Pay = Transition{From: NEW, To: PAY}
 	Pay2End = Transition{From: PAY, To: END}
+	End2End = Transition{From: END, To: END}
 )
 
 var PayFsm = FSM{
@@ -19,5 +20,6 @@ var PayFsm = FSM{
 	Transitions: map[string]Transition{
 		New2Pay.GetName(): New2Pay,
 		Pay2End.GetName(): Pay2End,
+		End2End.GetName(): End2End,
 	},
 }
