24f8f7c3389106501f502182acd943d8a518ea8a
David Xiang <admin@mdavid.cn>
2024-07-10 00:14:58 +0800

    upt

--- internal/state_handler.go
+++ internal/state_handler.go
@@ -3,35 +3,29 @@ package internal
 import (
 	"github.com/HEUDavid/go-fsm-demo/internal/pkg"
 	. "github.com/HEUDavid/go-fsm/pkg/metadata"
-	"github.com/HEUDavid/go-fsm/pkg/util"
 	"log"
 )
 
 var (
-	NEW = New{State: State{Name: "New", IsFinal: false}}
-	PAY = Pay{State: State{Name: "Pay", IsFinal: false}}
-	END = State{Name: "End", IsFinal: true}
+	New = GenState("New", false, newHandler)
+	Pay = GenState("Pay", false, payHandler)
+	End = State[*MyExtData]{Name: "End", IsFinal: true, ReHandle: nil}
 )
 
-type New struct{ State }
+func newHandler(task *Task[*MyExtData]) error {
+	log.Printf("State: %s, ExtData: %s", task.State, pkg.Pretty(task.GetExtData()))
 
-func (s New) Handle(task *Task[ExtDataEntity]) error {
-	extData, _ := util.Assert[*MyExtData](task.ExtData)
-	log.Printf("State: %s, ExtData: %s", task.State, pkg.Pretty(extData))
-
-	extData.Comment = "Modified by state handler" // Update ExtData
-	task.State = PAY.GetName()                    // Switch to next state
+	task.ExtData.Comment = "Modified by newHandler" // Update ExtData
+	task.State = Pay.GetName()                      // Switch to next state
 	return nil
 }
 
-type Pay struct{ State }
-
-func (s Pay) Handle(task *Task[ExtDataEntity]) error {
+func payHandler(task *Task[*MyExtData]) error {
 	log.Printf("State: %s, ExtData: %s", task.State, pkg.Pretty(task.GetExtData()))
 
 	// Invoke RPC interfaces to perform certain operations
 	// ...
 
-	task.State = END.GetName() // Switch to next state
+	task.State = End.GetName() // Switch to next state
 	return nil
 }
