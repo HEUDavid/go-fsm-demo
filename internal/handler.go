38af0e5aa21f95587e153617bb414c29e1692e11
David Xiang <admin@mdavid.cn>
2024-07-10 00:58:36 +0800

    upt

--- internal/handler.go
+++ internal/handler.go
@@ -9,8 +9,21 @@ import (
 var (
 	New = GenState("New", false, newHandler)
 	Pay = GenState("Pay", false, payHandler)
-	End = State[*MyExtData]{Name: "End", IsFinal: true, ReHandle: nil}
+	End = State[*MyExtData]{Name: "End", IsFinal: true, Handler: nil}
 )
+
+var (
+	New2Pay = GenTransition(New, Pay)
+	Pay2End = GenTransition(Pay, End)
+	End2End = GenTransition(End, End)
+)
+
+var PayFsm = func() FSM[*MyExtData] {
+	fsm := GenFSM(New)
+	fsm.RegisterState(New, Pay, End)
+	fsm.RegisterTransition(New2Pay, Pay2End, End2End)
+	return fsm
+}()
 
 func newHandler(task *Task[*MyExtData]) error {
 	log.Printf("State: %s, ExtData: %s", task.State, pkg.Pretty(task.GetExtData()))
