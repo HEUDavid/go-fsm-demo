72d786352e17c3ce9c7b802e87ec0b840c4af636
David Xiang <admin@mdavid.cn>
2024-07-10 11:07:07 +0800

    upt

--- model/fsm.go
+++ model/fsm.go
@@ -4,29 +4,29 @@ import (
 	"time"
 )
 
-// ExtData 拓展字段表，必须，根据具体业务设计字段
-type ExtData struct {
-	ID              int64   `gorm:"autoIncrement:true;primaryKey;column:id;type:bigint(20);not null"`
+// Data 业务字段表，必须，根据具体业务设计字段
+type Data struct {
+	ID              uint64  `gorm:"autoIncrement:true;primaryKey;column:id;type:bigint(20) unsigned;not null"`
 	TaskID          string  `gorm:"index:idx_task_id;column:task_id;type:char(32);not null"`
 	Symbol          string  `gorm:"column:symbol;type:varchar(20);not null;default:''"`
 	Quantity        float64 `gorm:"column:quantity;type:decimal(50,15);not null;default:0.000000000000000"`
 	Amount          float64 `gorm:"column:amount;type:decimal(50,15);not null;default:0.000000000000000"`
 	Operator        string  `gorm:"column:operator;type:varchar(20);not null;default:''"`
-	TransactionTime int64   `gorm:"column:transaction_time;type:bigint(20);not null;default:0;comment:'业务时间'"` // 业务时间
-	Comment         string  `gorm:"column:comment;type:varchar(128);not null;default:'';comment:'备注说明'"`       // 备注说明
+	TransactionTime uint64  `gorm:"column:transaction_time;type:bigint(20) unsigned;not null;default:0;comment:'业务时间'"` // 业务时间
+	Comment         string  `gorm:"column:comment;type:varchar(128);not null;default:'';comment:'备注说明'"`                // 备注说明
 }
 
 // TableName get sql table name.获取数据库表名
-func (m *ExtData) TableName() string {
-	return "ext_data"
+func (m *Data) TableName() string {
+	return "data"
 }
 
 // Task 任务主表，必须，维护状态驱动执行
 type Task struct {
 	ID         string    `gorm:"primaryKey;column:id;type:char(32);not null"`
-	RequestID  string    `gorm:"unique;column:request_id;type:char(32);not null;comment:'初始请求ID'"` // 初始请求ID
-	Type       string    `gorm:"column:type;type:varchar(128);not null;comment:'业务类型'"`            // 业务类型
-	State      string    `gorm:"column:state;type:varchar(128);not null;comment:'任务状态'"`           // 任务状态
+	RequestID  string    `gorm:"unique;column:request_id;type:char(32);not null;comment:'初始请求ID'"`       // 初始请求ID
+	Type       string    `gorm:"column:type;type:varchar(128);not null;comment:'业务类型'"`                  // 业务类型
+	State      string    `gorm:"index:idx_state;column:state;type:varchar(128);not null;comment:'任务状态'"` // 任务状态
 	Version    int       `gorm:"column:version;type:int(11);not null;default:1"`
 	CreateTime time.Time `gorm:"column:create_time;type:timestamp;not null;default:current_timestamp()"`
 	UpdateTime time.Time `gorm:"column:update_time;type:timestamp;not null;default:current_timestamp()"`
@@ -39,7 +39,7 @@ func (m *Task) TableName() string {
 
 // UniqueRequest 防重表，必须，创建更新操作对成功幂等
 type UniqueRequest struct {
-	ID         int64     `gorm:"autoIncrement:true;primaryKey;column:id;type:bigint(20);not null"`
+	ID         uint64    `gorm:"autoIncrement:true;primaryKey;column:id;type:bigint(20) unsigned;not null"`
 	RequestID  string    `gorm:"unique;column:request_id;type:char(32);not null;comment:'对成功幂等'"` // 对成功幂等
 	TaskID     string    `gorm:"column:task_id;type:char(32);not null"`
 	CreateTime time.Time `gorm:"column:create_time;type:timestamp;not null;default:current_timestamp()"`
