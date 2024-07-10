6e5a0a9c51da408a8223a50ca0548e4ec3d65db1
David Xiang <admin@mdavid.cn>
2024-07-10 11:25:35 +0800

    upt

--- internal/handler.go
+++ internal/handler.go
@@ -9,7 +9,7 @@ import (
 var (
 	New = GenState("New", false, newHandler)
 	Pay = GenState("Pay", false, payHandler)
-	End = State[*MyExtData]{Name: "End", IsFinal: true, Handler: nil}
+	End = State[*MyData]{Name: "End", IsFinal: true, Handler: nil}
 )
 
 var (
@@ -18,25 +18,30 @@ var (
 	End2End = GenTransition(End, End)
 )
 
-var PayFsm = func() FSM[*MyExtData] {
+var PayFSM = func() FSM[*MyData] {
 	fsm := GenFSM(New)
 	fsm.RegisterState(New, Pay, End)
 	fsm.RegisterTransition(New2Pay, Pay2End, End2End)
 	return fsm
 }()
 
-func newHandler(task *Task[*MyExtData]) error {
-	log.Printf("State: %s, ExtData: %s", task.State, _pretty(task.GetExtData()))
+func newHandler(task *Task[*MyData]) error {
+	log.Printf("State: %s, Data: %s", task.State, _pretty(task.GetData()))
 
-	task.ExtData.Comment = "Modified by newHandler" // Update ExtData
-	task.State = Pay.GetName()                      // Switch to next state
+	// It may be necessary to perform some checks.
+	// It may be necessary to pre-record the request to the database to ensure idempotency.
+	// For example, generating some request IDs.
+	// ...
+
+	task.Data.Comment = "Modified by newHandler" // Update Data
+	task.State = Pay.GetName()                   // Switch to next state
 	return nil
 }
 
-func payHandler(task *Task[*MyExtData]) error {
-	log.Printf("State: %s, ExtData: %s", task.State, _pretty(task.GetExtData()))
+func payHandler(task *Task[*MyData]) error {
+	log.Printf("State: %s, Data: %s", task.State, _pretty(task.GetData()))
 
-	// Invoke RPC interfaces to perform certain operations
+	// Invoke RPC interfaces to perform certain operations.
 	// ...
 
 	task.State = End.GetName() // Switch to next state
@@ -44,6 +49,6 @@ func payHandler(task *Task[*MyExtData]) error {
 }
 
 func _pretty(v interface{}) string {
-	b, _ := json.MarshalIndent(v, "", "  ")
-	return string(b)
+	s, _ := json.MarshalIndent(v, "", "  ")
+	return string(s)
 }
