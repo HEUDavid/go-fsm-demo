78daad68621271a02e6b03d833445734a6cf35cc
David Xiang <admin@mdavid.cn>
2024-07-10 12:32:07 +0800

    upt

--- cmd/service.go
+++ cmd/service.go
@@ -45,7 +45,7 @@ func _response(c *gin.Context, err error, task interface{}) {
 	if err == nil {
 		c.JSON(http.StatusOK, &task)
 	} else {
-		c.JSON(http.StatusOK, map[string]error{"error": err})
+		c.JSON(http.StatusOK, map[string]string{"error": err.Error()})
 	}
 }
 
